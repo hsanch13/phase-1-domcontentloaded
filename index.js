88655242313acf18c11a55bbbabdefa095cd838f
hsanch13 <hayleysanchez13@gmail.com>
2023-11-22 22:57:05 -0600

    add Com Content Loaded

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+   const p = document.querySelector("p")
+   p.textContent = "This is really cool!"
+    // console.log("The DOM has loaded");
+    // console.log(document.querySelector('p'))
+  });
+
+//   console.log("This is really cool!")
+//   console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered");
+//   console.log(document.querySelector('p'))
